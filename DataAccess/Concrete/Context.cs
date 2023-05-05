1f5438f5034cf7158766ab9496c7e1d3597a873a
Enes Başarslan <enes.basarslan@gmail.com>
2023-05-05 10:47:05 +0300

    Identity-mig

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;initial catalog=EasyCashDb;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> customerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> customerAccountProcesses { get; set; }
+    }
+}
